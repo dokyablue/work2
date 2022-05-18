e0dcc804aa1c21b1c5bd4a0ae629d5c7e1c7e1ce
dokyablue <anjsi0@gmail.com>
2022-05-18 21:04:58 +0900

    show videos

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,28 @@
+import { useState , useEffect } from 'react';
 import './app.css';
+import VideoList from './component/video_list/video_list';
 
 function App() {
+  const [videos,setVideos] = useState([]);
+
+  const [name , setName ] = useState('kkh');
+
+  useEffect(()=>{
+    console.log('useEffect');
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBooXZSq_tSvK2YsJ8dVFL0v9eJYHbla_8", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+
+  },[]);
   return (
-    <h1>HELLO</h1>
+    <VideoList videos={videos} />
   );
 }
 
