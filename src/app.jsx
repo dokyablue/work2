2494a5ce855343b36bd06d111c1bfa0ae72dc70b
dokyablue <anjsi0@gmail.com>
2022-05-18 23:29:44 +0900

    refactor

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,27 @@
 import { useState , useEffect } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchBar from './component/search_bar/search_bar';
 import VideoList from './component/video_list/video_list';
 
-function App() {
+function App({youtube}) {
   const [videos,setVideos] = useState([]);
 
-  const [name , setName ] = useState('kkh');
-
+  const search = query => {
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
+  };
   useEffect(()=>{
     console.log('useEffect');
-
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBooXZSq_tSvK2YsJ8dVFL0v9eJYHbla_8", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));    
   },[]);
   return (
-    <VideoList videos={videos} />
+    <div className={styles.app}>
+      <SearchBar onSearch={search}/>
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
---
+++ src/component/search_bar/search_bar.jsx
@@ -0,0 +1,34 @@
+import React , {useRef} from 'react';
+import styles from './search_bar.module.css';
+
+const SearchBar = ({onSearch}) => {
+    const onClick = () =>{
+        handleSearch();
+    };
+    const onKeyPress = (event) =>{
+        if(event.key === 'Enter'){
+            handleSearch();
+        }
+    };
+
+    const inputRef = useRef();
+    const handleSearch = () =>{
+        const value = inputRef.current.value;
+        onSearch(value);
+    };
+    return (
+
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img  className={styles.img} src="/images/logo.png" alt="logo" />
+                <h1 className={styles.title} >Youtube</h1>
+            </div>
+            <input className={styles.input}  type="search" placeholder='search!' onKeyPress={onKeyPress} ref={inputRef} />
+            <button className={styles.button}  type='submit' onClick={onClick}>
+                <img className={styles.buttonImg} src="/images/search.png" alt="search" />
+            </button>
+        </header>
+            )
+};
+
+export default SearchBar;
