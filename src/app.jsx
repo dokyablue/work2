dbaf990f54dba32693d39ecc7d953e029b747a4d
dokyablue <anjsi0@gmail.com>
2022-05-21 21:22:05 +0900

    detail

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,22 @@ import { useState , useEffect } from 'react';
 import styles from './app.module.css';
 import SearchBar from './component/search_bar/search_bar';
 import VideoList from './component/video_list/video_list';
+import VideoDetail from './component/video_detail/video_detail';
 
 function App({youtube}) {
   const [videos,setVideos] = useState([]);
+  const [selectedVideo,setSelectedVideo] = useState(null);
 
+  const selectVideo  = (video) => {
+    setSelectedVideo(video);
+  };
   const search = query => {
+    setSelectedVideo(null);
     youtube
     .search(query)
-    .then(videos => setVideos(videos));
+    .then(videos => {
+      setVideos(videos);
+    });
   };
   useEffect(()=>{
     console.log('useEffect');
@@ -20,7 +28,17 @@ function App({youtube}) {
   return (
     <div className={styles.app}>
       <SearchBar onSearch={search}/>
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {
+          selectedVideo &&  (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo?'list':'grid'} />
+        </div>
+      </section>
     </div>
   );
 }
